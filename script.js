b3505cbf46fbc44a9dbd38fb374fae1d86e91839
elodieboerma <elodieb@comcast.net>
2025-10-15 21:35:39 -0500

    create function getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!")
+function getComputerChoice() {
+    // randomly return rock, paper, or scissors; use Math.random(); NO arrays necessary for this game; test using console.log()/devtools
+    let choice = Math.random();
+    if (choice <= 0.33) {
+        return "rock";
+    } else if (choice <= 0.67) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+// console.log(getComputerChoice());
