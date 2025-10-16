6a9653f5c4ca48ad4e76be12feb713658f6d7e3e
elodieboerma <elodieb@comcast.net>
2025-10-15 22:10:55 -0500

    write code for winner scenarios in playRound

--- script.js
+++ script.js
@@ -24,24 +24,28 @@ function playRound(humanChoice,computerChoice) {
     // IF human and computer tie
     if (humanChoice === computerChoice) {
         console.log("Tie! No points for anyone!");
-    }
     // IF human choice is rock
-    if (humanChoice === "rock") {
-        // IF computer choice is paper
-            // human wins - rock beats scissors
-        // ELSE computer choice is scissors
-            // human wins - rock beats scissors
-    }
+    } else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You win! Rock beats paper!");
+        } else {
+            console.log("You win! Rock beats scissors!");
+        }
     // IF human choice is paper
-        // IF computer choice is rock
-            // computer wins - rock beats paper
-        // ELSE computer choice is scissors
-            // computer wins - scissors beat paper
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats paper!");
+        } else {
+            console.log("You lose! Scissors beat paper!");
+        }
     // IF human choice is scissors
-        // IF computer choice is rock
-            // computer wins - rock beats scissors
-        // ELSE computer choice is paper
-            // human wins - scissors beat paper
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats scissors!");
+        } else (computerChoice === "paper") {
+            console.log("You win! Scissors beat paper!");
+        }
+    }
     // based on winner - store winner to a variable
         //console.log message based on winner, ie:
             // console.log("You lose! Paper beats rock!");
